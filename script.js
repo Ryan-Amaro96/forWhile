cef3b0be5fa94b9866ea24d20a1cbd4041679ec5
Ryan Amaro <ryanamaro2003.st@gmail.com>
2022-06-14 11:24:11 -0300

    fírst commit

---
+++ script.js
@@ -0,0 +1,21 @@
+for(let count = 1; count <= 10; count++) {
+    console.log(count);
+}
+
+let regCount = 10
+while(regCount>=0){
+    console.log(regCount)
+    regCount-=1
+}
+
+let pair = 0
+while(pair<=100){
+    console.log(pair)
+    pair +=2
+}
+
+let unpaired = 1
+while(unpaired<=100){
+    console.log(unpaired)
+    unpaired +=2
+}
